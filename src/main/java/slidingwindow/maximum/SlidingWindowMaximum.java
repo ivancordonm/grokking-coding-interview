858becf3ce93ac75aa35be7fd8534629325a2c00
Ivan <ivancordonm@gmail.com>
2023-04-11 23:09:31 +0200

    java, maximum in sliding window

---
+++ src/main/java/slidingwindow/maximum/SlidingWindowMaximum.java
@@ -0,0 +1,9 @@
+package slidingwindow.maximum;
+
+public class SlidingWindowMaximum {
+
+    public int[] findMaxSlidingWindow(int[] nums, int w) {
+        // your code will replace the placeholder return statement below
+        return new int[0];
+    }
+}
