fc036dfa50229659099712355547e26d26866f2c
Ivan <ivancordonm@gmail.com>
2023-04-12 22:45:04 +0200

    java, maximum in sliding window. First attempt

--- src/main/java/slidingwindow/maximum/SlidingWindowMaximum.java
+++ src/main/java/slidingwindow/maximum/SlidingWindowMaximum.java
@@ -1,9 +1,26 @@
 package slidingwindow.maximum;
 
+import java.util.ArrayDeque;
+
 public class SlidingWindowMaximum {
 
     public int[] findMaxSlidingWindow(int[] nums, int w) {
-        // your code will replace the placeholder return statement below
-        return new int[0];
+        var result = new int[nums.length - w + 1];
+        var currentWindow = new ArrayDeque<Integer>();
+
+        for (var i = 0; i <= nums.length - w; i++) {
+            for (var j = i; j < w; j++) {
+                if(!currentWindow.isEmpty() && currentWindow.getFirst() < j) currentWindow.removeFirst();
+                if(currentWindow.isEmpty()) currentWindow.add(j);
+                else {
+                    if(currentWindow.getLast() < nums[j]) {
+                        currentWindow.removeLast();
+                    }
+                    currentWindow.addLast(nums[j]);
+                }
+            }
+            result[i] = currentWindow.getFirst();
+        }
+        return result;
     }
 }
