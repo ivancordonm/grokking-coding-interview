f303f983d84694a337a566e77503b67ff8609514
Ivan <ivancordonm@gmail.com>
2023-04-22 10:12:11 +0200

    O(N*M) solution. I've removed a while loop that made the solution O(N^2)

--- src/main/java/slidingwindow/minwindowsubsequence/MinWindow.java
+++ src/main/java/slidingwindow/minwindowsubsequence/MinWindow.java
@@ -1,29 +1,27 @@
 package slidingwindow.minwindowsubsequence;
 
 public class MinWindow {
-    public String getMinimumString(String s, String t) {
-        var indexS = 0;
-        var indexT = 0;
+    public String getMinimumString(String str1, String str2) {
+        var indexStr1 = 0;
+        var indexStr2 = 0;
         var minStr = "";
-        while (indexS < s.length()) {
-            while (indexT < t.length() && indexS < s.length()) {
-                if(t.charAt(indexT) == s.charAt(indexS)) indexT++;
-                indexS++;
-            }
-            // I'm now at the end of str2. You may or may not have a match
-            if(indexT == t.length()) {
-                indexT --;
-                var start = indexS - 1;
-                while(indexT >= 0) {
-                    if(t.charAt(indexT) == s.charAt(start)) indexT--;
+        while (indexStr1 < str1.length()) {
+            if (str2.charAt(indexStr2) == str1.charAt(indexStr1)) indexStr2++;
+            if (indexStr2 == str2.length()) {
+                indexStr2--;
+                var start = indexStr1;
+                var end = indexStr1 + 1;
+                while (indexStr2 >= 0) {
+                    if (str2.charAt(indexStr2) == str1.charAt(start)) indexStr2--;
                     start--;
                 }
                 start++;
-                indexT++;
-                if(minStr.isEmpty() || minStr.length() > indexS - start) {
-                    minStr = s.substring(start, indexS);
+                indexStr2 = 0;
+                if (minStr.isEmpty() || minStr.length() > end - start) {
+                    minStr = str1.substring(start, end);
                 }
             }
+            indexStr1++;
         }
         return minStr;
     }
