b4b0da1c948f8fd38f14a315f1b495d59fbb1555
Ivan <ivancordonm@gmail.com>
2023-04-29 10:16:51 +0200

    Al small improvement minimizing the condition

--- src/main/java/mergeintervals/mergeintervals/MergeInterval.java
+++ src/main/java/mergeintervals/mergeintervals/MergeInterval.java
@@ -9,7 +9,9 @@ public class MergeInterval {
         output.add(new Interval(intervals.get(0).getStart(), intervals.get(0).getEnd()));
         for (var interval : intervals.subList(1, intervals.size())) {
             var lastInterval = output.get(output.size() - 1);
-            if (interval.getStart() >= lastInterval.getStart() && interval.getStart() <= lastInterval.getEnd()) {
+            // Because the intervals are sorted, we can just check if the start of the current interval
+            //
+            if (interval.getStart() <= lastInterval.getEnd()) {
                 lastInterval.setEnd(Math.max(interval.getEnd(), lastInterval.getEnd()));
             } else {
                 output.add(new Interval(interval.getStart(), interval.getEnd()));
