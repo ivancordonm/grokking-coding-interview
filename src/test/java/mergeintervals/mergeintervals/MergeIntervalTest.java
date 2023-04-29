3adede0049e408dcc4684e0c3ccae5bd905c3328
Ivan <ivancordonm@gmail.com>
2023-04-29 10:28:30 +0200

    modify intervals input format and its converter

--- src/test/java/mergeintervals/mergeintervals/MergeIntervalTest.java
+++ src/test/java/mergeintervals/mergeintervals/MergeIntervalTest.java
@@ -12,8 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MergeIntervalTest {
 
     @CsvSource(value = {
-            "1,5,3,7,4,6;1,7",
-            "1,5,4,6,6,8,11,15;1,8,11,15"
+            "1:5,3:7,4:6;1:7",
+            "1:5,4:6,6:8,11:15;1:8,11:15"
     }, delimiterString = ";")
     @ParameterizedTest(name = "mergeIntervals({0}) = {1}")
     void mergeIntervals(@ConvertWith(IntervalsConverter.class) List<Interval> intervals,
--- src/test/java/utils/IntervalsConverter.java
+++ src/test/java/utils/IntervalsConverter.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ArgumentConverter;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class IntervalsConverter implements ArgumentConverter {
 
@@ -17,10 +17,8 @@ public class IntervalsConverter implements ArgumentConverter {
                     "The argument should be a string: " + source);
         }
 
-        var string = s.split(",");
-        return IntStream.range(0, string.length / 2)
-                .mapToObj(i -> new Interval(Integer.parseInt(string[i * 2]), Integer.parseInt(string[i * 2 + 1])))
+        return Arrays.stream(s.split(",")).map(interval -> interval.split(":"))
+                .map(interval -> new Interval(Integer.parseInt(interval[0]), Integer.parseInt(interval[1])))
                 .collect(Collectors.toList());
-
     }
 }
