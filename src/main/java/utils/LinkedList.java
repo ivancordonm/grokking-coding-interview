038edee98637db3ab49b994a9e98bffd838ee9fc
Ivan <ivancordonm@gmail.com>
2023-05-10 19:14:55 +0200

    implements Iterable on LinkedList and LinkedListNode. The latter is because in exercise reverse it returns the head of the list

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -1,16 +1,17 @@
 package utils;
 
-public class LinkedList<T> {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+
+public class LinkedList<T> implements Iterable<T>{
 
     public LinkedListNode<T> head;
 
-    // constructor will be used to make a LinkedList type object
     public LinkedList() {
         this.head = null;
     }
 
-    // insertNodeAtHead method will insert a LinkedListNode at head
-    // of a linked list.
     public void insertNodeAtHead(LinkedListNode<T> node) {
         if (this.head != null) {
             node.next = this.head;
@@ -18,8 +19,6 @@ public class LinkedList<T> {
         this.head = node;
     }
 
-    // createLinkedList method will create the linked list using the
-    // given integer array with the help of InsertAthead method.
     public void createLinkedList(T[] lst) {
         for (int i = lst.length - 1; i >= 0; i--) {
             LinkedListNode<T> newNode = new LinkedListNode<>(lst[i]);
@@ -27,4 +26,29 @@ public class LinkedList<T> {
         }
     }
 
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedListIterator<>(this);
+    }
+
+    static class LinkedListIterator<T> implements Iterator<T> {
+        private LinkedListNode<T> current;
+
+        public LinkedListIterator(LinkedList<T> list) {
+            this.current = list.head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.current != null;
+        }
+
+        @Override
+        public T next() {
+            T data = this.current.data;
+            this.current = this.current.next;
+            return data;
+        }
+    }
 }
--- src/main/java/utils/LinkedListNode.java
+++ src/main/java/utils/LinkedListNode.java
@@ -1,6 +1,10 @@
 package utils;
 
-public class LinkedListNode<T> {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+
+public class LinkedListNode<T> implements Iterable<T>{
 
     public T data;
     public LinkedListNode<T> next;
@@ -16,4 +20,30 @@ public class LinkedListNode<T> {
         return (next != null) ? data + " -> " + next : data.toString();
     }
 
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedListNodeIterator<>(this);
+    }
+
+    public static class LinkedListNodeIterator<T> implements Iterator<T> {
+
+        private LinkedListNode<T> node;
+
+        public LinkedListNodeIterator(LinkedListNode<T> node) {
+            this.node = node;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return node != null;
+        }
+
+        @Override
+        public T next() {
+            T data = node.data;
+            node = node.next;
+            return data;
+        }
+    }
 }
