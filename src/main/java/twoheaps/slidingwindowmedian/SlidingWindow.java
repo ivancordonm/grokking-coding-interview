74dd8f02db1d5b5a164c876492ae6183feaced9b
Ivan <ivancordonm@gmail.com>
2023-07-24 17:22:39 +0200

    Remove traces

--- src/main/java/twoheaps/slidingwindowmedian/SlidingWindow.java
+++ src/main/java/twoheaps/slidingwindowmedian/SlidingWindow.java
@@ -20,66 +20,42 @@ public class SlidingWindow {
         var balance = maxHeap.size() == minHeap.size() ? 0 : 1;
         var left = 0;
         var result = new double[nums.length - k + 1];
+
+        // It can be a hashmap as well
         ArrayList<Integer> removed = new ArrayList<>();
 
         result[left] = getMedian(maxHeap, minHeap, balance);
-        System.out.println("Result: " + result[left]);
         for (var i = k; i < nums.length; i++) {
-            System.out.println("------------------------------------");
-            System.out.println("Before adding:");
-            System.out.println("MaxHeap: " + maxHeap);
-            System.out.println("MinHeap: " + minHeap);
-            System.out.println("Balance: " + balance);
-            System.out.println("Element to add: " + nums[i]);
             assert !maxHeap.isEmpty(); // maxHeap is never empty
             if (nums[i] <= maxHeap.peek()) {
-                System.out.println("Adding to maxHeap");
                 maxHeap.add(nums[i]);
                 balance++;
             } else {
-                System.out.println("Adding to minHeap");
                 minHeap.add(nums[i]);
                 balance--;
             }
-            System.out.println("------------------");
-            System.out.println("After adding:");
-            System.out.println("MaxHeap: " + maxHeap);
-            System.out.println("MinHeap: " + minHeap);
-            System.out.println("Balance: " + balance);
             var elementToRemove = nums[left++];
-            System.out.println("Element to remove: " + elementToRemove);
             if (!minHeap.isEmpty() && elementToRemove == minHeap.peek()) {
-                System.out.println("Removing from minHeap");
                 minHeap.poll();
                 balance++;
             } else if (!maxHeap.isEmpty() && elementToRemove == maxHeap.peek()) {
-                System.out.println("Removing from maxHeap");
                 maxHeap.poll();
                 balance--;
             } else if (removed.contains(minHeap.peek())) {
-                System.out.println("Removing from minHeap (already removed)");
                 minHeap.poll();
                 balance++;
                 removed.remove(minHeap.peek());
             } else if (removed.contains(maxHeap.peek())) {
-                System.out.println("Removing from maxHeap (already removed)");
                 maxHeap.poll();
                 balance--;
                 removed.remove(maxHeap.peek());
             } else {
                 removed.add(elementToRemove);
-                System.out.println("Adding to removed: " + elementToRemove);
                 if (!maxHeap.isEmpty() && elementToRemove < maxHeap.peek())
                     balance--;
                 else
                     balance++;
             }
-
-            System.out.println("------------------");
-            System.out.println("After removing:");
-            System.out.println("MaxHeap: " + maxHeap);
-            System.out.println("MinHeap: " + minHeap);
-            System.out.println("Balance: " + balance);
 
             if (balance > 1) {
                 minHeap.add(maxHeap.poll());
@@ -88,14 +64,8 @@ public class SlidingWindow {
                 maxHeap.add(minHeap.poll());
                 balance += 2;
             }
-            System.out.println("------------------");
-            System.out.println("After balancing:");
-            System.out.println("MaxHeap: " + maxHeap);
-            System.out.println("MinHeap: " + minHeap);
-            System.out.println("Balance: " + balance);
 
             result[left] = getMedian(maxHeap, minHeap, balance);
-            System.out.println("Result: " + result[left]);
 
         }
         return result;
