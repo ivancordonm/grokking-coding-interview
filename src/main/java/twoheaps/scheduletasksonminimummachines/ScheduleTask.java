89e2a804d6b4389eeb38d02375b996447077493e
Ivan <ivancordonm@gmail.com>
2023-07-27 22:17:41 +0200

    remove traces

--- src/main/java/twoheaps/scheduletasksonminimummachines/ScheduleTask.java
+++ src/main/java/twoheaps/scheduletasksonminimummachines/ScheduleTask.java
@@ -1,6 +1,5 @@
 package twoheaps.scheduletasksonminimummachines;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -17,35 +16,18 @@ public class ScheduleTask {
         for (var task : tasksList) {
             taskQueue.offer(new int[]{task.get(0), task.get(1)});
         }
-        System.out.println("TaskQueue: " + printQueue(taskQueue));
-        System.out.println("----------------------------------");
         while (!taskQueue.isEmpty()) {
             var task = taskQueue.poll();
-            System.out.println("Task: " + Arrays.toString(task));
             int[] machine;
             if (!machineQueue.isEmpty() && task[0] >= machineQueue.peek()[0]) {
-                System.out.println("MachineQueue peek " + Arrays.toString(machineQueue.peek()));
                 machineQueue.poll();
             } else {
                 numMachine++;
             }
             machine = new int[]{task[1], numMachine};
             machineQueue.offer(machine);
-            System.out.println("machine " + Arrays.toString(machine));
-            System.out.println("machineQueue " + printQueue(machineQueue));
-            System.out.println("----------------------------------");
         }
         return numMachine;
     }
 
-    private String printQueue(PriorityQueue<int[]> queue) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int[] interval : queue) {
-            sb.append(interval[0] + ":" + interval[1] + " ");
-        }
-
-        return sb.toString();
-    }
 }
