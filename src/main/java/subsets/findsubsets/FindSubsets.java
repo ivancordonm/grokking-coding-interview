d901caf6bea6a3abf8d339f9697824f893cb57d5
Ivan <ivancordonm@gmail.com>
2023-09-16 18:45:00 +0200

    add comments

--- src/main/java/subsets/findsubsets/FindSubsets.java
+++ src/main/java/subsets/findsubsets/FindSubsets.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class FindSubsets {
 
+    // We make a bit mask between the number of set (0..2^n) and the index of the element
+    // i.e. in the set 3 (011) the element with index 1 and 2 are present. (index is counted from 1 to n)
     private int getBit(int num, int bit) {
         int temp = (1 << bit);
         temp = temp & num;
@@ -14,6 +16,7 @@ public class FindSubsets {
         return 1;
     }
 
+    // The time complexity of this algorithm is O(n * 2^n), where n is the number of elements in the input set.
     public List<List<Integer>> findAllSubsets(int[] nums) {
         List<List<Integer>> setsList = new ArrayList<>();
         if (nums.length != 0) {
